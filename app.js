f0a87ffc3de381a76b32821e84410815a65f27c6
resalat123 <resalatamin@gmail.com>
2019-12-11 14:42:03 +0600

    use of type js

---
+++ app.js
@@ -0,0 +1,14 @@
+const typed = new Typed('.test', {
+    
+    strings: [
+        'Resalat Amin',
+        '22 years old',
+        'a web developer',
+      ],
+      typeSpeed: 40,
+      backSpeed: 40,
+      backDelay: 1500,
+      loop: true,
+  });
+
+
